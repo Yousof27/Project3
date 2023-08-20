75f9513da3601584cac6565750d4a4ea2c9e2e4d
Yousof <yousofkamal111@gmail.com>
2023-08-20 03:45:28 +0300

    creat project

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+var copyIcon = document.querySelector (".p-3 .text-area-con .icon-con i")
+var delIcon = document.getElementById ("#delBtn")
+var commentArea = document.querySelector ("textarea")
+
+function copy() {
+    alert ("The text has been copied")
+}
+copyIcon.onclick = copy
+
+
